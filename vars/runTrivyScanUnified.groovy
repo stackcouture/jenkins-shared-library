0dc779423475b70cd227f3ac50f8a4dd756af539
Naveen <naveenramlu28@gmail.com>
2025-08-20 06:28:29 +0530

    #updated the trivyscan

--- vars/runTrivyScanUnified.groovy
+++ vars/runTrivyScanUnified.groovy
@@ -1,22 +1,16 @@
-def runTrivyScan(Map config = [:]) {
-    def stageName = config.stageName ?: error("Missing 'stageName'")
-    def scanTarget = config.scanTarget ?: error("Missing 'scanTarget'")
-    def scanType = config.scanType ?: error("Missing 'scanType'")
-    def fileName = config.fileName
-
+def call(String stageName, String scanTarget, String scanType) {
     if (!['image', 'fs'].contains(scanType)) {
         error("Invalid scanType: ${scanType}. Allowed values are 'image' or 'fs'.")
     }
 
     def reportDir = "reports/trivy/${env.BUILD_NUMBER}/${stageName}"
-
     def htmlReport = scanType == 'fs' 
-        ? "${reportDir}/trivy-fs-scan-${fileName}.html"
-        : "${reportDir}/trivy-image-scan-${fileName}.html"
+        ? "${reportDir}/trivy-fs-scan-${env.COMMIT_SHA}.html"
+        : "${reportDir}/trivy-image-scan-${env.COMMIT_SHA}.html"
 
     def jsonReport = scanType == 'fs' 
-        ? "${reportDir}/trivy-fs-scan-${fileName}.json"
-        : "${reportDir}/trivy-image-scan-${fileName}.json"
+        ? "${reportDir}/trivy-fs-scan-${env.COMMIT_SHA}.json"
+        : "${reportDir}/trivy-image-scan-${env.COMMIT_SHA}.json"
 
     sh """
         set -e
