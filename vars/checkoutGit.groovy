9239880f811cb6a0ba2bdf6b4a6966d361977970
Naveen <naveenramlu28@gmail.com>
2025-08-21 01:15:15 +0530

    #196 Update

--- vars/checkoutGit.groovy
+++ vars/checkoutGit.groovy
@@ -1,24 +1,14 @@
-// vars/checkoutGit.groovy
 def call(String gitBranch, String gitUrl, String secretName) {
-    echo "Fetching AWS secrets..."
-    def secrets = getAwsSecret(secretName, 'ap-south-1')
-
-    echo "Secrets retrieved: ${secrets}" 
     
-    if (secrets == null) {
-        error("Failed to retrieve secrets from AWS for path: ${secretName}")
+    withCredentials([usernamePassword(credentialsId: 'github-credentials-id', usernameVariable: 'GITHUB_USER', passwordVariable: 'GITHUB_PAT')]) {
+        echo "Using GitHub PAT for git operations"
+        checkout([
+            $class: 'GitSCM',
+            branches: [[name: "*/${gitBranch}"]],
+            userRemoteConfigs: [[
+                url: gitUrl, 
+                credentialsId: 'github-credentials-id'
+            ]]
+        ])
     }
-    if (!secrets.github_pat) {
-        error("GitHub PAT not found in AWS secrets at path: ${secretName}")
-    }
-
-    def credentialsId = secrets.github_pat
-
-    echo "Checking out branch '${gitBranch}' from '${gitUrl}' using provided credentials."
-
-    checkout([
-        $class: 'GitSCM',
-        branches: [[name: "*/${gitBranch}"]],
-        userRemoteConfigs: [[url: gitUrl, credentialsId: credentialsId]]
-    ])
 }
