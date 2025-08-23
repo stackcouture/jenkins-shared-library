0721dfef668fcef6aecf4864b798885864e37789
Naveen <naveenramlu28@gmail.com>
2025-08-24 01:01:00 +0530

    #update the directory

--- vars/buildDockerImage.groovy
+++ vars/buildDockerImage.groovy
@@ -1,3 +1,6 @@
 def call(String imageTag) {
-    sh "docker build --no-cache -t ${imageTag} ."
+    dir("${env.WORKSPACE}/java-app") {
+        sh "ls -lh target/*.jar || (echo 'JAR file not found!' && exit 1)"
+        sh "docker build --no-cache -t ${imageTag} ."
+    }
 }
