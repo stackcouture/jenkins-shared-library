a3efa8736c3fc11f0cd5d5e7fc95df233da05d5c
Naveen <naveenramlu28@gmail.com>
2025-08-24 00:47:03 +0530

    #update the directory

--- vars/buildDockerImage.groovy
+++ vars/buildDockerImage.groovy
@@ -1,5 +1,3 @@
 def call(String imageTag) {
-    dir('source') {
         sh "docker build --no-cache -t ${imageTag} ."
-    }
 }
--- vars/checkoutGit.groovy
+++ vars/checkoutGit.groovy
@@ -2,16 +2,13 @@ def call(String gitBranch, String gitUrl, String secretName) {
     
     withCredentials([usernamePassword(credentialsId: 'github-credentials-id', usernameVariable: 'GITHUB_USER', passwordVariable: 'GITHUB_PAT')]) {
         echo "Using GitHub PAT for git operations"
-        sh 'rm -rf source'
-        dir('source') {
-            checkout([
-                $class: 'GitSCM',
-                branches: [[name: "*/${gitBranch}"]],
-                userRemoteConfigs: [[
-                    url: gitUrl,
-                    credentialsId: 'github-credentials-id'
-                ]]
-            ])
-        }
+        checkout([
+            $class: 'GitSCM',
+            branches: [[name: "*/${gitBranch}"]],
+            userRemoteConfigs: [[
+                url: gitUrl, 
+                credentialsId: 'github-credentials-id'
+            ]]
+        ])
     }
 }
